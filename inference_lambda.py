65ed0b3f5868e2f3c4f485931823680d771ba3e9
HurryRabbit <72260167+HurryRabbit@users.noreply.github.com>
2021-05-24 21:59:27 +0800

    Update inference_lambda.py

--- inference_lambda.py
+++ inference_lambda.py
@@ -5,11 +5,8 @@ import time
 import cv2
 import base64
 import hashlib
-from os import listdir
-import boto3
 
 import os
-import csv
 import torch
 from torch import nn
 from torchvision import models
@@ -22,39 +19,29 @@ s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     ####### PUT YOUR INFORMATION HERE #######
-    CAPTAIN_EMAIL = 'jason2650@gmail.com'   #
+    CAPTAIN_EMAIL = 'your_email'            #
     SALT = 'my_salt'                        #
     #########################################
     
+    # Get current time
     now = int(time.time())
     
+    # Generate server uuid
     server_uuid_unconverted = CAPTAIN_EMAIL + SALT
     server_uuid = generate_server_uuid(server_uuid_unconverted)
     
+    # Transform base64 codes to image
     image_nparray = base64_to_image(event['image'])
     
-    # upload to s3
-    if event['esun_uuid'] == 'test':
-        upload_outcome = 'no upload'
-    else:
-        upload_outcome = image_to_s3(image_nparray)
-        
-    #inference
-    #answer_str = inference(image_nparray)
+    # Inference
+    answer = predict(image_nparray)
 
-    ############# TESTING CODES #############
-    
-    dirlist = listdir("/mnt/ml/model")
-    
-    #########################################
-
+    # Function Output = API Output
     return {  
     "esun_uuid": event['esun_uuid'],
     "server_uuid": server_uuid,
     "server_timestamp": now,
-    "answer": "test"
-    #,"test_output (upload to s3)": upload_outcome
-    ,"test_output (listdir)": dirlist
+    "answer": answer
     }
     
 def generate_server_uuid(input_string):
@@ -72,23 +59,31 @@ def generate_server_uuid(input_string):
     return server_uuid
 
 def base64_to_image(image_64_encoded):
-    img_binary = base64.b64decode(image_64_encoded)
-    image_bgr = cv2.imdecode(np.frombuffer(img_binary, np.uint8), cv2.IMREAD_COLOR)
-    image_rgb = cv2.cvtColor(image_bgr,cv2.COLOR_BGR2RGB)
+    """ Convert base64 to numpy.ndarray for cv2.
 
-    return image_rgb
+    @param:
+        image_64_encode(str): image that encoded in base64 string format.
+    @returns:
+        image(numpy.ndarray): an image.
+    """
+    img_binary = base64.b64decode(image_64_encoded)
+    image = cv2.imdecode(np.frombuffer(img_binary, np.uint8), cv2.IMREAD_COLOR)
+
+    return image
     
-def image_to_s3(image_nparray):
-    image = Image.fromarray(image_nparray)
+def predict(image):
+    """ Predict your model result.
+
+    @param:
+        image (numpy.ndarray): an image.
+    @returns:
+        prediction (str): a word.
+    """
+
+    ####### PUT YOUR MODEL INFERENCING CODE HERE #######
     
-    try:
-        im.save("/mnt/ml/temp.jpg")
-        
-        now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        s3_file_path = 'API-pic-input/' + now_str + '.jpg'
-        s3.meta.client.upload_file(Bucket='tbrain-2021-images', Key=s3_file_path, Filename="/mnt/ml/temp.jpg")
-        upload_outcome = 'success'
-    except:
-        upload_outcome = 'failed'
-    
-    return upload_outcome
+
+
+    ####################################################
+    if _check_datatype_to_string(prediction):
+        return prediction
